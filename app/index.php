c1b701716e12f34f864574c0644960e816122e75
Phil Wood <philwood@phils-mbp.home>
2017-02-09 08:58:23 +0000

    Changed Helper to static class

--- app/index.php
+++ app/index.php
@@ -14,11 +14,11 @@ $host 		= $url->getHost();
 $paths 		= $url->getPaths();
 $query 		= $url->getQuery();
 
-$helper 	= new Helper();
+$helper 	= Helper::Instance();
 
-$helper->raw($full_url);
-$helper->raw($urlParts);
-$helper->raw($scheme);
-$helper->raw($host);
-$helper->raw($paths);
-$helper->raw($query);
+$helper::raw($full_url);
+$helper::raw($url_parts);
+$helper::raw($scheme);
+$helper::raw($host);
+$helper::raw($paths);
+$helper::raw($query);
